2ab97472b011d6a4612b874ebf0c8c4abdbf9a03
AnastasiiaMorozova64 <prettyanastasia64@gmail.com>
2025-05-23 01:43:53 +0300

    Create a method to add products to the cart  or update their quantity

--- cactus_shop/cart/cart.py
+++ cactus_shop/cart/cart.py
@@ -15,3 +15,21 @@ class Cart(object):
             # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+  
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                    'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # Оновлення сесії cart
+        self.session[settings.CART_SESSION_ID] = self.cart
+        # Відмітити сеанс як змінений, щоб впевнитись, що він збережений
+        self.session.modified = True
