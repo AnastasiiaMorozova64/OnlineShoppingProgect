e718f102230f37df4104e332ed057c78ee88f102
AnastasiiaMorozova64 <prettyanastasia64@gmail.com>
2025-05-23 02:02:00 +0300

    Create method for clearing a cart session

--- cactus_shop/cart/cart.py
+++ cactus_shop/cart/cart.py
@@ -69,4 +69,6 @@ class Cart(object):
         return sum(Decimal(item['price']) * item['quantity'] for item in
                 self.cart.values())
     
-    
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.session.modified = True
