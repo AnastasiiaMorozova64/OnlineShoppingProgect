453acd5df303c634bc7ef5ec0671d5bf607522f2
AnastasiiaMorozova64 <prettyanastasia64@gmail.com>
2025-05-23 01:34:19 +0300

    Create cart.py with changing settings

--- cactus_shop/cactus_shop/settings.py
+++ cactus_shop/cactus_shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_SESSION_ID = 'cart'
